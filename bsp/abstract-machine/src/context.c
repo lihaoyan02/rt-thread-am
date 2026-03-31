71e8a919404319ccca523a75dd761763ccecb4a9
100018556-Li Haoyan <1620774239@qq.com>
2026-03-31 10:00:14 +0800

    feat: able to execute based on am

--- bsp/abstract-machine/src/context.c
+++ bsp/abstract-machine/src/context.c
@@ -2,8 +2,16 @@
 #include <klib.h>
 #include <rtthread.h>
 
+static volatile rt_ubase_t ctx_from; 
+static volatile rt_ubase_t ctx_to; 
+static int from_flag;
+
 static Context* ev_handler(Event e, Context *c) {
   switch (e.event) {
+		case EVENT_YIELD: 
+			if (from_flag) *(Context**)ctx_from = c;
+			c = *(Context**)ctx_to;  
+											break;
     default: printf("Unhandled event ID = %d\n", e.event); assert(0);
   }
   return c;
@@ -14,10 +22,17 @@ void __am_cte_init() {
 }
 
 void rt_hw_context_switch_to(rt_ubase_t to) {
+	ctx_to = to;
+	from_flag = 0;
+	yield();
   assert(0);
 }
 
 void rt_hw_context_switch(rt_ubase_t from, rt_ubase_t to) {
+	ctx_from = from;
+	ctx_to = to;
+	from_flag = 1;
+	yield();
   assert(0);
 }
 
@@ -26,6 +41,8 @@ void rt_hw_context_switch_interrupt(void *context, rt_ubase_t from, rt_ubase_t t
 }
 
 rt_uint8_t *rt_hw_stack_init(void *tentry, void *parameter, rt_uint8_t *stack_addr, void *texit) {
-  assert(0);
-  return NULL;
+	uintptr_t stack_top = (uintptr_t)stack_addr & ~(sizeof(uintptr_t) -1);
+	Context *cp = kcontext((Area) { NULL, (rt_uint8_t*) stack_top}, tentry, parameter);
+	cp->gpr[1] = (uintptr_t)texit;	
+  return (rt_uint8_t*)cp;
 }
